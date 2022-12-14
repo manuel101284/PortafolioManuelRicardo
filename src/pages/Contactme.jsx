27c1f680fd6b44d3590bb691f51c189018d0ea70
manuel101284 <manuel101284@gmail.com>
2022-12-14 01:37:25 -0500

    V.02. Avance formularios

--- src/pages/Contactme.jsx
+++ src/pages/Contactme.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const Contactme = () => {
+  const [inputText, setInputText] = useState('');
+  
+  const captureName = (e) => {
+    const personName = e.target.value
+    setInputText(personName);
+    console.log(personName);
+  }
+
+
   return (
-    <section className="container container-04">
-        <input type="text" placeholder="name"></input>
-        <input type="email" placeholder="email"></input>
-        <input type="text" placeholder="subject"></input>
-        <input type="text" placeholder="message"></input>
-        <div className="btn btn-SEND"><span>SEND</span></div>
+    <section className="container" id="container-04">
+        <input id="textName" type="text" placeholder="name" onChange={ captureName }></input>
+        <input id="textEmail" type="email" placeholder="email"></input>
+        <input id="textSubject" type="text" placeholder="subject"></input>
+        <textarea id="textMessage" type="text" placeholder="message" rows={5} cols={20}></textarea>
+        <button id="btnSend" class="bg-green-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-blue-500 rounded">SEND</button>
     </section>
   )
 }
