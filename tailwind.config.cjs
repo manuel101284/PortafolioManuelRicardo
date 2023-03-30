4d74e44694aa855fb456890b92c191cfb861670d
manuel101284 <manuel101284@gmail.com>
2023-03-30 01:34:16 -0500

    V.03. Rediseño del portafolio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./dist/**/*.{html,js,ts,jsx,tsx}",],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0f172a',
+        // primary: '#ecfccb'
+      },    
+      fontFamily: {
+        Roboto: ['Roboto', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [],
 }
