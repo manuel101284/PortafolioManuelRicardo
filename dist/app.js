cc4e453ff396f51f2c426264184c734389e3e7c0
manuel101284 <manuel101284@gmail.com>
2023-03-31 01:49:53 -0500

    V.04. Estructra completa, próximo paso: contenido y estilo propio

---
+++ dist/app.js
@@ -0,0 +1,91 @@
+const menuButton = document.querySelector("#menu-button");
+const menuItems = document.querySelector("#menu");
+const menuButtonSpans = document.querySelectorAll('#menu-button span');
+const links = document.querySelectorAll("#menu a");
+
+// OPEN MENU
+
+menuButton.addEventListener('click', () => {
+    menuItems.classList.toggle('hidden');
+
+    menuButtonSpans.forEach((span) =>{
+        span.classList.toggle('animate');
+    }) 
+});
+
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        menuItems.classList.add('hidden')
+        menuButtonSpans.forEach((span) => {
+            span.classList.remove('animate')
+        })
+    })
+})
+
+// CLOSE MENU WHEN MAXIMIZE
+window.onresize = function () {
+    let w = window.outerWidth
+    if (w > 768) {
+        menuItems.classList.add('hidden')
+        menuButtonSpans.forEach((span) => {
+            span.classList.remove('animate')
+        })
+    }
+}
+
+// DARK MODE
+
+const darkButton = document.querySelector('#darkButton');
+
+darkButton.addEventListener('click', function () {
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+        // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+});
+
+//  VARIABLES JOB SECTION
+const jobSection = document.querySelector('#jobSection');
+const articles = document.querySelectorAll('#jobSection article');
+
+jobSection.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+
+    if (id) {
+        articles.forEach((article) => {
+            article.classList.add('hidden');
+        })
+        const element = document.getElementById(id);
+        element.classList.remove('hidden');
+    }
+})
+
+const jobLinks = document.querySelectorAll('#jobSection button');
+const firstLink = document.querySelector('#link1');
+
+jobLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        firstLink.classList.remove(
+            'border-sky-500',
+            'bg-white',
+            'dark:bg-slate-700'
+        )
+    })
+})
